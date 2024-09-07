6f7d1117bfe9c8edd0824dde07a172721e685a6e
Navi4347 <naveenkonda.dev@gmail.com>
2024-09-07 12:39:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return <div>
+    <div className="container">
+  <div className="row">
+    <div className="col-sm">
+      One of three columns
+    </div>
+    <div className="col-sm">
+      One of three columns
+    </div>
+    <div className="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+  </div>;
+}
+
+export default App;
